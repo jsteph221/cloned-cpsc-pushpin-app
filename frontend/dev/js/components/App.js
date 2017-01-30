7d1ed69078d36f4a5088e603e5a9b7f36144fc88
Supercareful <supercareful@gmail.com>
2017-01-29 21:42:50 -0800

    basic frontend with fabric setup

--- frontend/dev/js/components/App.js
+++ frontend/dev/js/components/App.js
@@ -1,16 +1,63 @@
+// import React from 'react';
+// import UserList from '../containers/user-list';
+// import UserDetails from '../containers/user-detail';
+// 
+
+
+// const App = () => (
+//     <section className = "mainApp">
+    	
+
+//     	<div className = "toolbar">
+//     		Toolbar here
+//     	</div>
+
+//     	<div className = "fabricApp">
+//     		Insert canvas here
+//         </div>
+
+//         <div className = "mapApp">
+//         	Insert map here
+//         </div>
+//     </section>
+// );
+
+
+
+// export default App;
+
+
 import React from 'react';
-import UserList from '../containers/user-list';
-import UserDetails from '../containers/user-detail';
+import {Canvas, Text} from 'react-fabricjs';
 require('../../scss/style.scss');
 
-const App = () => (
-    <div>
-        <h2>User List</h2>
-        <UserList />
-        <hr />
-        <h2>User Details</h2>
-        <UserDetails />
-    </div>
-);
+const HelloFabric = React.createClass({
+  render: function() {
+    return (
+    		<section className = "mainApp">
 
-export default App;
+    			<div className = "fabricApp">
+			      <Canvas
+			        width="950"
+			        height="900">
+			          <Text
+			            text="Fabric App!!"
+			            left={300}
+			            top={300}
+			            fill="#000000"
+			            fontFamily="Arial"
+			          />
+			      </Canvas>
+			    </div>
+
+			    <div className = "mapApp">
+			    	Insert map here
+			    </div>
+
+		    </section>
+
+    );
+  }
+});
+
+export default HelloFabric;
