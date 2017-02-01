585514c864357a9eb6f416a08e62ed8f89fa0945
Salman Khan <supercareful@gmail.com>
2017-02-01 12:16:19 -0800

    Add files via upload

--- frontend/dev/js/components/App.js
+++ frontend/dev/js/components/App.js
@@ -27,9 +27,15 @@
 // export default App;
 
 
-import React from 'react';
-import {Canvas, Text} from 'react-fabricjs';
+import React, {Component} from 'react';
+import fabric, {Canvas, Text, IText, Circle, Path} from 'react-fabricjs';
+import GoogleMap from "react-google-map"
+import GoogleMapLoader from "react-google-maps-loader"
 require('../../scss/style.scss');
+
+var canvasWidth = screen.width/2;
+var canvasHeight = screen.height*0.9;
+
 
 const HelloFabric = React.createClass({
   render: function() {
@@ -38,16 +44,27 @@ const HelloFabric = React.createClass({
 
     			<div className = "fabricApp">
 			      <Canvas
-			        width="950"
-			        height="900">
-			          <Text
+			        width={canvasWidth}
+			        height="800">
+			          	<Text
 			            text="Fabric App!!"
 			            left={300}
 			            top={300}
 			            fill="#000000"
 			            fontFamily="Arial"
-			          />
-			      </Canvas>
+			    		/>
+
+	            		<Path
+		                path="M 0 0 L 300 100 L 200 300 z"
+		                fill="red"
+		                stroke="green"
+		                strokeWidth={10}
+		                opacity={0.5}
+	           			 />	
+
+	           			 <fabric.Triangle
+	           			 fill="pink"/>			
+					</Canvas>
 			    </div>
 
 			    <div className = "mapApp">
@@ -59,5 +76,7 @@ const HelloFabric = React.createClass({
     );
   }
 });
+
+
 
 export default HelloFabric;
