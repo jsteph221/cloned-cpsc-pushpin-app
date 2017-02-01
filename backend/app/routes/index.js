8b426d372dcb7aac2d8cac454e9545ed4092ad1a
Andrew Kim <taeyeonkim90@gmail.com>
2017-02-01 13:07:11 -0800

    separated out db and aws into config directory. added user model and verified model creation/retrieval working

--- backend/app/routes/index.js
+++ backend/app/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.send("This is the index page");
+  res.send("This is the root url of API router");
 });
+
+router.get('/setup', function(req, res) {
+  // create a sample user
+  var nick = new User({ 
+    name: 'Nick Cerminara', 
+    password: 'password',
+    admin: true 
+  });
+
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+router.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+}); 
 
 module.exports = router;
