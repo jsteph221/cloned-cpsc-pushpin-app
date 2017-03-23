b5406f32bf37046433b033c864b49a735ed07ddf
Andrew Kim <taeyeonkim90@gmail.com>
2017-03-23 12:31:29 -0700

    disabled allow-crednetial header from the backend. disabled withCredentials on Project get request from the front

--- frontend/dev/js/containers/imageLibrary.js
+++ frontend/dev/js/containers/imageLibrary.js
@@ -265,7 +265,7 @@ class ImageLibrary extends Component {
 
         var request = new XMLHttpRequest();
 
-        request.withCredentials = true;
+        /*request.withCredentials = true;*/
 
         request.open("GET", server+"/api/projects", false);
         request.send(null);
