2a207fab7a4216dbae262588b4f97a6f7761fcb3
jstephens221 <jstephens221@gmail.com>
2017-11-27 21:00:33 -0800

    changed access control origin

--- backend/app/app.js
+++ backend/app/app.js
@@ -16,7 +16,7 @@ var AWS = require('aws-sdk');
 
 // set CORS
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000');
+  res.setHeader("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
